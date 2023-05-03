2ca6e0babc163131e8759f5907af52057219a0c7
Leonid Mihaylov <leonmixt@gmail.com>
2023-05-03 16:34:32 +0400

    Changed methods for methods from helper

--- src/test/java/api/tests/CommonContactEmailTest.java
+++ src/test/java/api/tests/CommonContactEmailTest.java
@@ -1,29 +1,16 @@
 package api.tests;
 
-import api.email.EmailsApi;
 import api.helpers.EmailHelper;
-import io.restassured.response.Response;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class CommonContactEmailTest {
-    EmailsApi emailsApi = new EmailsApi();
     EmailHelper emailHelper = new EmailHelper();
     int contactId = 4909;
 
     @Test
     public void createEditDeleteContactEmailTest() {
-        emailsApi.createEmail(201, contactId);
-        Response response = emailsApi.getAllEmails(200, contactId);
-        int emailId = response.jsonPath().getInt("[0].id");
-        String email = response.jsonPath().getString("[0].email");
-        Assert.assertEquals(email, emailsApi.randomDataBodyForCreateEmail(contactId).getEmail(), "Created emails is not equals");
-
-        emailsApi.editExistingEmail(200, emailId, contactId);
-        Response editedEmails = emailsApi.getAllEmails(200, contactId);
-        String editedEmail = editedEmails.jsonPath().getString("[0].email");
-        Assert.assertEquals(editedEmail, emailsApi.randomDataBodyForEditEmail(emailId, contactId).getEmail(), "Edited emails is not equals");
-
+        Integer emailId = emailHelper.createEmail(contactId);
+        emailHelper.editEmail(emailId, contactId);
         emailHelper.deleteEmail(emailId);
     }
 }
